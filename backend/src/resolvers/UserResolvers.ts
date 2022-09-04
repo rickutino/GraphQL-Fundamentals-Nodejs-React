08145938fadc53646b34dcaeb3a59b511c7564c3
rickutino <rickutino@gmail.com>
2022-09-04 16:18:59 +0900

    add Resolver User, query all user  and Mutations new user.

--- backend/src/resolvers/UserResolvers.ts
+++ backend/src/resolvers/UserResolvers.ts
@@ -1,10 +1,27 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+import crypto from "crypto";
 
 @Resolver()
 export class UserResolver {
-  
-  @Query(() => String)
-  async hello() {
-    return "Hello World"
+  private data: User[] = [];
+
+  //Query: get date
+  // Mutation: create, patch, delete data
+
+  @Query(() => [User])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg('name') name: string
+  ) {
+    const user = { id: crypto.randomUUID(), name }
+
+    this.data.push(user);
+
+    return user;
   }
 }
